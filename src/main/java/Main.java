c3e1574e347cfaecdb17f4ea1bfeea4d32185e2e
moredarker <dm.kaftailov@gmail.com>
2022-11-16 01:24:58 +0300

    some replace by regex

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,53 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    public static void main(String[] args) {
-        //String line = "<client>(Какие то данные)<data>Иванов Иван Иванович;79991113344;my_mail@gmail.com</data></client>";
-        //String line = "<client>(Какие то данные)<data>79991113344;test@yandex.ru;Иванов Иван Иванович</data></client>";
-        //String line = "<client>(Какие то данные)<data></data></client>";
-        String line = "<client>(Какие то данные)<data>Иванов Иван Иванович;79991113344</data></client>";
-
+    static String maskData(String line) {
         Pattern tag = Pattern.compile("<data>(.*?)</data>");
         Matcher data = tag.matcher(line);
-
         String result = data.find() ? data.group(1) : "";
 
         Pattern split = Pattern.compile("[^;]+");
         Matcher items = split.matcher(result);
-        StringBuilder output = new StringBuilder();
+        List<String> output = new ArrayList<>();
 
         while (items.find()) {
             String current = items.group();
             String firstChar = current.substring(0, 1);
 
             if (Pattern.matches("\\d", firstChar)) {
-                String temp = current.substring(0, 4) + "***" + current.substring(7);
-                output.append(temp + ";");
+                output.add(current.replaceFirst("(\\d{4})(\\d{3})(\\d*)", String.format("$1%s$3", "*".repeat(3))));
             } else if (Pattern.matches("[А-Я]", firstChar)) {
                 String[] names = current.split(" ");
                 String lastName = names[0];
-                String temp = lastName.substring(0, 1) + "*".repeat(lastName.length() - 2) + lastName.substring(lastName.length() - 1);
-                names[0] = temp;
-                String fatherName = names[2].charAt(0) + ".";
-                names[2] = fatherName;
-                output.append(String.join(" ", names) + ";");
+                names[0] = lastName.charAt(0) + "*".repeat(lastName.length() - 2) + lastName.substring(lastName.length() - 1);
+                names[2] = names[2].charAt(0) + ".";
+                output.add(String.join(" ", names));
             } else if (Pattern.matches("[a-z]", firstChar)) {
                 String[] parts = current.split("[@]+");
                 StringBuilder left = new StringBuilder(parts[0]);
                 left.setCharAt(left.length() - 1, '*');
                 String right = "*".repeat(parts[1].indexOf("."));
-                output.append(left + "@" + right + parts[1].substring(parts[1].indexOf(".")));
+                output.add(left + "@" + right + parts[1].substring(parts[1].indexOf(".")));
             }
         }
 
-        Pattern beforeData = Pattern.compile(".+?(?=<data>)");
-        Matcher left = beforeData.matcher(line);
-        result = left.find() ? left.group() : "";
-        result += "<data>" + output + "</data>";
-        Pattern afterData = Pattern.compile("(?<=</data>).*$");
-        Matcher right = afterData.matcher(line);
-        result += right.find() ? right.group() : "";
-        System.out.println(result);
+        String out = String.join(";", output);
+        if (out.isEmpty()) return line;
+        return line.replaceFirst("(.+(?<=<data>))(.+(?=</data>))(.+)", String.format("$1%s$3", out));
     }
 }
---
+++ src/main/test/java/MainTest.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+
+public class MainTest {
+    @ParameterizedTest
+    @MethodSource("lines")
+    void maskData(String line, String expected) {
+        String result = Main.maskData(line);
+
+        assertEquals(expected, result);
+    }
+
+    static Stream<Arguments> lines() {
+        return Stream.of(Arguments.of(
+                "<client>(Какие то данные)<data>79991113344;test@yandex.ru;Иванов Иван Иванович</data></client>",
+                "<client>(Какие то данные)<data>7999***3344;tes*@******.ru;И****в Иван И.</data></client>"),
+                Arguments.of(
+                "<client>(Какие то данные)<data></data></client>",
+                "<client>(Какие то данные)<data></data></client>"
+                ),
+                Arguments.of(
+                "<client>(Какие то данные)<data>Иванов Иван Иванович;79991113344</data></client>",
+                "<client>(Какие то данные)<data>И****в Иван И.;7999***3344</data></client>"
+                ),
+                Arguments.of(
+                "<client>(Какие то данные)<data>Иванов Иван Иванович;79991113344;my_mail@gmail.com</data></client>",
+                "<client>(Какие то данные)<data>И****в Иван И.;7999***3344;my_mai*@*****.com</data></client>")
+                );
+    }
+}
