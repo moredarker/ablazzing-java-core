7114034bbf3e703c458ce76606bae097a94a5685
moredarker <dm.kaftailov@gmail.com>
2022-11-16 01:50:39 +0300

    rebuild dirs

--- src/lesson-1-expert/java/Main.java
+++ src/lesson-1-expert/java/Main.java
@@ -4,7 +4,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    static String maskData(String line) {
+    public static void main(String[] args) {
+        String line = "<client>(Какие то данные)<data>my.mail@rambler.ru;Сидоров Василий Петрович;79876453210</data></client>";
+        System.out.println(maskData(line));
+    }
+
+    public static String maskData(String line) {
         Pattern tag = Pattern.compile("<data>(.*?)</data>");
         Matcher data = tag.matcher(line);
         String result = data.find() ? data.group(1) : "";
--- src/lesson-1-expert/test/MainTest.java
+++ src/lesson-1-expert/test/MainTest.java
@@ -3,6 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
 import java.util.stream.Stream;
 
 public class MainTest {
